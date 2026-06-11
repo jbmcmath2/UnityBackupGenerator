dd8a153bd5174caf687f56121498bc9575afbc36
jbmcmath2 <43012443+jbmcmath2@users.noreply.github.com>
2026-06-11 12:03:00 -0500

    Changed source and destination variables parameters, changed file naming
    Changed the source and destination variables to be parameters rather than hardcoded file paths.
    
    Changed back-up naming scheme to use date and time, this helps with multiple back-ups in one day.

--- UnityBackupGenerator.ps1
+++ UnityBackupGenerator.ps1
@@ -1,6 +1,9 @@
-$todaysDate = Get-Date -Format MM-dd-yyyy
-$sourceFolder = "FolderA"
-$destFolder = "FolderB"
+param(
+    $sourceFolder,
+    $destFolder
+)
+$sourceName = Split-Path -Path $sourceFolder -Leaf
+$todaysDate = Get-Date -Format MM-dd-yyyy-HH-mm-ss
 
 if(!(Test-Path $destFolder)){
     Write-Error "Destination folder does not exist"
@@ -9,13 +12,13 @@ if(!(Test-Path $destFolder)){
 else{
     Write-Output "Destination folder valid"
 }
-if(Test-Path $destFolder\$todaysDate-back-up){
+if(Test-Path $destFolder\$sourceName-$todaysDate-back-up){
     Write-Error "Destination folder contains a back-up with the same name"
     Exit
 }
 else{
-    New-Item -ItemType Directory -Path $destFolder -Name $todaysDate-back-up
-    robocopy $sourceFolder $destFolder\$todaysDate-back-up /MIR /mt /e /z /log+:$destFolder\$todaysDate-back-up\$todaysdate.txt /XD "Library" "Temp" "Logs" ".vs" ".vscode"
+    New-Item -ItemType Directory -Path $destFolder -Name $sourceName-$todaysDate-back-up
+    robocopy $sourceFolder $destFolder\$sourceName-$todaysDate-back-up /MIR /mt /e /z /log+:$destFolder\$sourceName-$todaysDate-back-up\$todaysdate.txt /XD "Library" "Temp" "Logs" ".vs" ".vscode"
     Write-Output "Mirror Complete"
 }
-Write-Host "Unity Mirror Script Complete" -ForegroundColor Green
+Write-Host "Unity Back-up Script Complete" -ForegroundColor Green
